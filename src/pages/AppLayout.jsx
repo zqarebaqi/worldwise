a2fdbb3c53e25a48ea51c2ee296129a2b96f9f17
zqarebaqi <zqarebaqi@gmail.com>
2025-05-04 01:34:46 +0330

    Fix Map import

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,6 +1,6 @@
 import SideBar from '../components/SideBar';
 import styles from './AppLayout.module.css'
-import Map from '../components/map';
+import Map from '../components/Map';
 const AppLayout = () => {
     return (
         <div className={styles.app}>
